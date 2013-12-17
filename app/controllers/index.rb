cb9d54c0c42c0e2081dd487d2afec3fb47b90f2f
Tara Roys <tlroys@gmail.com>
2013-12-17 08:40:33 -0600

    added recipes view. Added recipes route.

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -4,3 +4,12 @@ get '/' do
   # Look in app/views/index.erb
   erb :index
 end
+
+ get '/recipes/' do
+  erb :recipes
+ end
+
+get '/recipes/:recipe_id' do 
+  @recipe = Recipe.where(id: params[:recipe_id]).first
+  erb :recipe 
+end 
